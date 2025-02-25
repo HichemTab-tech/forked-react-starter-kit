2702cd7f394e260327ab729e93838bc4157ceb20
HichemTech <hichem.tab2002@gmail.com>
2025-02-25 10:51:04 +0100

    Replace Link with anchor tag for external GitHub link
    The Link component was replaced with a standard anchor tag for the external GitHub URL to ensure proper behavior of the `target="_blank"` attribute. This change improves compatibility and aligns with expected HTML standards for external links.

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -22,13 +22,13 @@ export default function Welcome() {
                             </Link>
                         ) : (
                             <>
-                                <Link
+                                <a
                                     target="_blank"
                                     href="https://github.com/HichemTab-tech"
                                     className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
                                 >
                                     HichemTab-tech
-                                </Link>
+                                </a>
                                 <Link
                                     href={route('register')}
                                     className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
